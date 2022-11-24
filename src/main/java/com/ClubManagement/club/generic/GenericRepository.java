0128e4a2712c54ee05a3c9a50f51a09e2fbb8c9e
ouertaniaziz <mohamedaziz.ouertani@esprit.tn>
2022-11-24 17:41:04 +0100

    Initial project

---
+++ src/main/java/com/ClubManagement/club/generic/GenericRepository.java
@@ -0,0 +1,9 @@
+package com.ClubManagement.club.generic;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface GenericRepository<T,ID> extends JpaRepository<T,ID> {
+
+}
